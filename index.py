63da2e705b5cc424a87592eafdbf2291c8f009a2
alxyz12 <naufalhhdd@gmail.com>
2026-01-20 08:31:00 +0700

    create variable and int

--- index.py
+++ index.py
@@ -1,3 +1,13 @@
 import tkinter as tk
 from tkinter import *
 import random
+
+win = tk.Tk()
+win.geometry("750x750")
+win.title("PythonGeeks")
+
+num = random.randint(1, 50)
+hint = StringVar()
+score = IntVar()
+final_score = IntVar()
+guess = IntVar()
